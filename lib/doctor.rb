7d897dc15996dc060477549795f4e3edc62b2d5f
Kevin Karma <kevinkarma55@gmail.com>
2019-08-31 23:52:52 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,7 +17,7 @@ class Doctor
     Appointment.all.select { |apt| apt.doctor == self } 
   end
   
-  def new_appointment(date,patient)
+  def new_appointment( patient, date )
     Appointment.new(date, patient, self) 
     binding.pry 
   end 
